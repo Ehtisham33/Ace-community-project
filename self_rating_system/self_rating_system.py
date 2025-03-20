6f02764654cf5d9e0123280db4bbdc810fda1a36
Ehtisham33 <educatedshami@gmail.com>
2025-03-20 12:17:52 +0500

    okkk done

--- self_rating_system/self_rating_system.py
+++ self_rating_system/self_rating_system.py
@@ -5,18 +5,31 @@ from typing import Dict, List
 
 app = FastAPI()
 
-
+# Load sports rating data from JSON file
 with open("assets/file.json", "r") as file:
     SPORTS_QUESTIONS = json.load(file)
 
 class RatingRequest(BaseModel):
-    sports: Dict[str, Dict[str, str]]  
+    sports: Dict[str, Dict[str, str]]  # Dictionary of sports with their answers
 
 @app.get("/questions/{sport}")
 def get_questions(sport: str):
     if sport not in SPORTS_QUESTIONS:
         raise HTTPException(status_code=400, detail="Sport not supported")
     return SPORTS_QUESTIONS[sport]
+
+@app.get("/questions")
+def get_multiple_questions(sports: str):
+    sports_list = sports.split(",")
+    questions = {}
+    
+    for sport in sports_list:
+        if sport in SPORTS_QUESTIONS:
+            questions[sport] = SPORTS_QUESTIONS[sport]
+        else:
+            raise HTTPException(status_code=400, detail=f"Sport '{sport}' not supported")
+    
+    return questions
 
 @app.post("/calculate_rating")
 def calculate_rating(request: RatingRequest):
