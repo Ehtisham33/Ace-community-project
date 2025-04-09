34b8307223f5fb9837b317a77d0b1f752331a761
Ehtisham33 <educatedshami@gmail.com>
2025-04-09 15:47:29 +0500

    api done

--- Padel-AI/padel_ai.py
+++ Padel-AI/padel_ai.py
@@ -1,93 +1,74 @@
 import os
-import streamlit as st
+from flask import Flask, jsonify, request
 import asyncio
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
-
+import warnings
+from langchain_huggingface import HuggingFaceEmbeddings,HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
 from langchain_core.prompts import PromptTemplate
-from langchain_huggingface import HuggingFaceEndpoint
-
-## Uncomment the following files if you're not using pipenv as your virtual environment manager
 from dotenv import load_dotenv, find_dotenv
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
 load_dotenv(find_dotenv())
+app = Flask(__name__)
+DB_FAISS_PATH =r"vectorstore/db_faiss"
 
+#load vectore store
+embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+vectorstore = FAISS.load_local(DB_FAISS_PATH,embedding_model,allow_dangerous_deserialization=True)
 
-DB_FAISS_PATH=r"D:\Glaxit Projects\Ace-community-project\Padel-AI\vectorstore\db_faiss"
-@st.cache_resource
-def get_vectorstore():
-    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-    db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
-    return db
+CUSTOM_PROMPT_TEMPLATE ="""
+Use the pieces of information provided in the context to answer user's question.
+If you don’t know the answer, just say that you don’t know, don’t try to make up an answer.
+Don’t provide anything out of the given context.
 
+Context: {context}
+Question: {question}
 
-def set_custom_prompt(custom_prompt_template):
-    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
-    return prompt
+Start the answer directly. No small talk please."""
 
+def set_custom_prompt():
+    return PromptTemplate(template=CUSTOM_PROMPT_TEMPLATE,input_variables=['context','questions'])
 
-def load_llm(huggingface_repo_id, HF_TOKEN):
-    llm = HuggingFaceEndpoint(
-        repo_id=huggingface_repo_id,
-        task="text-generation",  
-        temperature=0.5,
-        model_kwargs={"token": HF_TOKEN, "max_length": "512"}
+def load_llm():
+    HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
+    HF_TOKEN = os.environ.get("HF_TOKEN")
+
+    return HuggingFaceEndpoint(
+        repo_id = HUGGINGFACE_REPO_ID,
+        task = "test-generation",
+        temperature = 0.5,
+        model_kwargs={
+            "token":HF_TOKEN,
+            "max_length":"512"
+        }
     )
-    return llm
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(),
+    chain_type = "stuff",
+    retriever=vectorstore.as_retriever(search_kwargs={'k': 3}),
+    return_source_documents = True,
+    chain_type_kwargs = {'prompt': set_custom_prompt()}
+)
+
+@app.route('/ask',methods=['post'])
+def ask_questions():
+    try:
+        data = request.json
+        query = data.get('question', '')
+
+        if not query:
+            return jsonify({'error': 'No question provided'}), 400
+
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        response = qa_chain.invoke({'query': query})
+        return jsonify({'answer': response['result']})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
-
-def main():
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    st.title("Ask Chatbot!")
-
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        st.chat_message(message['role']).markdown(message['content'])
-
-    prompt=st.chat_input("Pass your prompt here")
-
-    if prompt:
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role':'user', 'content': prompt})
-
-        CUSTOM_PROMPT_TEMPLATE = """
-                Use the pieces of information provided in the context to answer user's question.
-                If you dont know the answer, just say that you dont know, dont try to make up an answer. 
-                Dont provide anything out of the given context
-
-                Context: {context}
-                Question: {question}
-
-                Start the answer directly. No small talk please.
-                """
-        
-        HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
-        HF_TOKEN=os.environ.get("HF_TOKEN")
-
-        try: 
-            vectorstore=get_vectorstore()
-            if vectorstore is None:
-                st.error("Failed to load the vector store")
-
-            qa_chain=RetrievalQA.from_chain_type(
-                llm=load_llm(huggingface_repo_id=HUGGINGFACE_REPO_ID, HF_TOKEN=HF_TOKEN),
-                chain_type="stuff",
-                retriever=vectorstore.as_retriever(search_kwargs={'k':3}),
-                return_source_documents=True,
-                chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
-            )
-
-            response=qa_chain.invoke({'query':prompt})
-
-            result=response["result"]
-            st.chat_message('assistant').markdown(result)
-            st.session_state.messages.append({'role':'assistant', 'content': result})
-
-        except Exception as e:
-            st.error(f"Error: {str(e)}")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=7005,debug=True)
